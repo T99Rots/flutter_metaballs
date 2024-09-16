0aa7d0e0826f95dad573335d2288a19372906416
T99Rots <T99.Rots@gmail.com>
2024-09-16 23:06:02 +0200

    moved web files

---
+++ lib/src/renderers/web/vertex_shader.dart
@@ -0,0 +1,10 @@
+const String vertexSource = r'''
+precision mediump float;
+
+attribute vec2 position;
+
+void main() {
+  // position specifies only x and y.
+  // We set z to be 0.0, and w to be 1.0
+  gl_Position = vec4(position, 0.0, 1.0);
+}''';
